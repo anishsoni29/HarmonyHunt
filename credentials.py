20346571ba0ab3b9238c0b3ed701f79475106f3c
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-21 02:28:42 +0530

    added the test file and connection string urls

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 #credentials.py
 
-DB_USER = 'your--username'
-DB_PASSWORD = ''
+DB_USER = 'sonianish441'
+DB_PASSWORD = 'postgresql://sonianish441:TK6bwckjlUS4@ep-twilight-waterfall-38593792.us-east-2.aws.neon.tech/test?sslmode=require'
