470a39ebefce6888f8428061956d706694862716
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-21 02:34:59 +0530

    imported from load dotenv

--- credentials.py
+++ credentials.py
@@ -1,4 +1,9 @@
-#credentials.py
+from dotenv import load_dotenv
+import os
 
-DB_USER = ''
-DB_PASSWORD = ''
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the database credentials from environment variables
+DB_USER = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
